037db338f1df273eed3ad5d6a48e1ce091dff8c2
ShubrajSalgotra <salgotrajshub56@gmail.com>
2025-02-11 21:03:51 +0530

    Added imaged for medicines

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,20 @@
 import Image1 from '../assets/hero-image.jpg'
+import CALIVEX from '../assets/img/calivex.png'
+import NERVEX from '../assets/img/NERVEX.png'
+import NERVEX25 from '../assets/img/NERVEX25.png'
+import RABIVEXD from '../assets/img/RABIVEXD.png'
+import VEXEZONE from '../assets/img/VEXEZONE.png'
+import VEXOFIT from '../assets/img/VEXOFIT.png'
+import VEXQ from '../assets/img/VEXQ.png'
 
 export const products = [
-  // {
-  //   id: 1,
-  //   name: 'VEXOFIT',
-  //   type: 'Syrup',
-  //   image: Image1,
-  //   composition: '',
-  // },
+  {
+    id: 1,
+    name: 'VEXOFIT',
+    type: 'Syrup',
+    image: VEXOFIT,
+    composition: '',
+  },
   {
     id: 2,
     name: 'VEXOTUS',
@@ -54,14 +61,14 @@ export const products = [
     id: 8,
     name: 'VEX-Q',
     type: 'SoftGel Capsules',
-    image: Image1,
+    image: VEXQ,
     composition: 'Co Enzyme Q10, 100 Mg + Omega 3 Fatty Acid 150 Mg + Lycopene+ L Carnitine + A to Z Multivitamins + Antioxidants',
   },
   {
     id: 9,
     name: 'VEXEZONE 1.5',
     type: 'Injection',
-    image: Image1,
+    image: VEXEZONE,
     composition: 'Cefaperozone 1000 Mg + Sulbactum 500 Mg',
   },
   // {
@@ -75,21 +82,21 @@ export const products = [
     id: 11,
     name: 'NERVEX',
     type: 'Capsules',
-    image: Image1,
+    image: NERVEX,
     composition: 'Methyalcobalamin 1500 Mcg + ALA 100 Mg + Folic Acid 1.5 Mg + Pyridoxine 3 Mg',
   },
   {
     id: 12,
     name: 'NERVEX 2.5',
     type: 'I.M/I.V Injection',
-    image: Image1,
+    image: NERVEX25,
     composition: 'Methyalcobalamin 2500 Mcg + Pyridoxine 100 Mg + Niacinamide 100 Mg',
   },
   {
     id: 13,
     name: 'RABIVEX-D',
     type: 'SR Capsules',
-    image: Image1,
+    image: RABIVEXD,
     composition: 'Rabeprazole 20 Mg + Domperidone 30 Mg',
   },
   {
@@ -103,7 +110,7 @@ export const products = [
     id: 15,
     name: 'CALIVEX-D3',
     type: 'Tablets',
-    image: Image1,
+    image: CALIVEX,
     composition: 'Calcium Citrate 1000 Mg +  Magnesium 100 Mg + Vitamin D3 200IU + Zinic 4 Mg',
   },
   {
